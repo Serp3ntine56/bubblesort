cb27317250a9a3b35259f2fa9bbff5dc7b08c3e7
Serp3ntine56 <samfedkova@gmail.com>
2025-11-25 13:24:46 +0000

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -2,19 +2,13 @@ import random
 y = 10
 items = [random.randint(0, 100) for x in range(y)]
 n = len(items)-1
-swapped = True
 index = 0
 
-while swapped and n > 0:
-    swapped = False
+for x in range(0, n):
     for index in range(0, n):
         if (items[index] > items[index+1]):
             temp = items[index]
             items[index] = items[index+1]
             items[index+1] = temp
-
-            swapped = True
-
-    n = n - 1
-
+            
 print(items)
